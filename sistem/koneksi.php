5f701e8b9a337761800a08761c7faa31dbfa260f
rezaoktario <46617648+rezaio@users.noreply.github.com>
2022-10-21 08:36:23 +0700

    Update koneksi.php
    update koneksi

--- sistem/koneksi.php
+++ sistem/koneksi.php
@@ -1,10 +1,10 @@
 <?php
 function open_connection()
 {
-    $hostname = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "20753028_akademik";
+    $hostname = "103.151.63.30";
+    $username = "kelompok4";
+    $password = ")Eh(hfhCEZ_.R**b";
+    $dbname = "kelompok4";
     $koneksi = mysqli_connect($hostname, $username, $password, $dbname);
     return $koneksi;
 }
